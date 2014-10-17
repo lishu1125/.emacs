56adad90f1cddf7e17439afb4c09d578c5a3a85a
lishu <lishu1125@126.com>
2014-10-17 21:32:23 +0800

    add file

---
+++ site-lisp/init-git-emacs.el
@@ -0,0 +1,9 @@
+﻿;;git-emacs配置
+(setq explicit-shell-file-name
+      "C:/Program Files/Git/bin/bash.exe")
+(setq shell-file-name explicit-shell-file-name)
+(add-to-list 'exec-path "C:/Program Files/Git/bin")
+(add-to-list 'load-path "~/.emacs.d/git-emacs/")
+(require 'git-emacs)
+
+(provide 'init-git-emacs)
---
+++ site-lisp/init-hs-minor-mode.el
@@ -0,0 +1,21 @@
+﻿;;hs-minor-mode配置
+;;hs-hide-all
+;;hs-show-all
+(setq hs-allow-nesting t)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (hs-minor-mode 1)
+            ))
+(add-hook 'emacs-lisp-mode-hook
+          (lambda()
+            (hs-minor-mode 1)))
+(add-hook 'tcl-mode-hook (lambda ()
+                           (hs-minor-mode 1)
+                           ))
+(add-to-list 'hs-special-modes-alist
+             '(c-mode "[\n\t ]*{" "}" "/[*/]" nil hs-c-like-adjust-block-beginning))
+(add-to-list 'hs-special-modes-alist
+             '(c++-mode "[\n\t ]*{" "}" "/[*/]" nil hs-c-like-adjust-block-beginning))						   
+(define-key global-map (kbd "M-,") 'hs-toggle-hiding)
+
+(provide 'init-hs-minor-mode)
