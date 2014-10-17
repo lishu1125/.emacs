60330244b070a0873231272de27936af925ddec6
lishu <lishu1125@126.com>
2014-10-17 17:26:03 +0800

    add file

---
+++ site-lisp/init-helm.el
@@ -0,0 +1,19 @@
+;;helm≈‰÷√
+;;(add-to-list 'load-path "~/.emacs.d/helm")
+;;(require 'helm-config)
+;;(global-set-key (kbd "M-x") 'helm-M-x)
+;;(global-set-key (kbd "C-x b") 'helm-mini)
+;;(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;;(global-set-key (kbd "C-c h e") 'helm-etags-select)
+;;(global-set-key (kbd "C-c h g") 'helm-do-grep)
+;;(global-set-key (kbd "C-c h s") 'helm-semantic-or-imenu)
+;;(global-set-key (kbd "C-c h m") 'helm-all-mark-rings)
+;;(global-set-key (kbd "C-c h f") 'helm-find)
+;;(global-set-key (kbd "C-c h l") 'helm-locate)
+;;(global-set-key (kbd "C-c h o") 'helm-occur)
+;;(global-set-key (kbd "C-c h r") 'helm-resume)
+;;(global-set-key (kbd "C-c h y") 'helm-show-kill-ring)
+;;(global-set-key (kbd "C-c h i") 'helm-register
+;;(helm-mode 1)
+
+(provide 'init-helm)
---
+++ site-lisp/init-imenu-anywhere.el
@@ -0,0 +1,5 @@
+;;imenu-anywhereÅäÖÃ
+(require 'imenu-anywhere)
+(global-set-key (kbd "C-.") 'imenu-anywhere)
+
+(provide 'init-imenu-anywhere)
